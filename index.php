950c19fab98fdf62cda4ccbb09838682c8c37ee2
tenpei999 <tenpei999@gmail.com>
2023-03-26 03:14:43 +0900

    [feat]c-hoge

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 <div class="grid place-items-center h-full">
-	<h1 class="text-5xl text-white font-bold tracking-widest c-drop-shadow--blue">THEMRISHvite</h1>
-	<div class="absolute bottom-1 left-1/2 -translate-x-1/2">
-		<p class="mb-4 text-xs text-white tracking-widest c-drop-shadow--blue">Currently in <strong><?php echo (IS_VITE_DEVELOPMENT) ? "development" : "production" ?></strong> mode.</p>
-	</div>
+	<h1>
+		<p class="c-hoge">ほげ</p>
+		<p class="c-hoge__huga">ふぐ</p>
+	</h1>
 </div>
 <?php get_footer(); ?>
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import './assets/styles/tailwind_config.css'
-import "./main.scss";
+// import "./main.scss";
 import './assets/scripts/main.js';
+import './assets/styles/scss/style.scss';
 import.meta.glob( [
 	'./assets/images/**',
 ] );
