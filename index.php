8a8039ba6e38f0688ca0f24588f3f24e5ba02e3b
tenpei999 <tenpei999@gmail.com>
2023-11-10 00:02:55 +0900

    main visuar 仮

--- index.php
+++ index.php
@@ -1,22 +1,23 @@
 <?php get_header(); ?>
 <main class="l-main">
 	<section class="p-main-visual w-screen h-screen flex items-center justify-center">
-		<ul class="flex flex-col items-center justify-center text-white">
+		<ul class="w-7/12 flex flex-col items-center justify-center text-white">
 			<li>
-				<h1 class="c-icatch text-center font-bold">
-					小論文で<br>
-					入試に勝つ!
+				<h1 class="c-icatch text-center tracking-wider font-bold">
+					小論文<span class="c-icatch__particle">で</span><br>
+					入試<span class="c-icatch__particle">に</span>勝つ!
 				</h1>
 			</li>
 			<li>
-				<p class="text-2xl">多数の学習参考書を執筆し、リクルート・スタディサプリ講師を務める代表・神﨑史彦が監修・指導する小論文塾（総合型・学校推薦型選抜対策塾）です。</p>
+				<p class="pt-7 text-2xl">多数の学習参考書を執筆し、リクルート・スタディサプリ講師を務める代表・神﨑史彦が監修・指導する小論文塾（総合型・学校推薦型選抜対策塾）です。</p>
 			</li>
 			<li>
-				<button class="c-button--gold c-button--gold">
-					<a href="#" class="p-4 ">
+				<button class="c-button--gold mt-8 flex">
+					<a href="#" class="block py-4 px-16 leading-8">
 					入塾希望の方はこちら
 					</a>
 				</button>
+				<span class="c-dli-arrow-right"></span>
 			</li>
 		</ul>
 
