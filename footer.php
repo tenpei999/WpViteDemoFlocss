a486bf8b563e5c6bcd895c9c0b1a9ccd6c59c146
tenpei999 <tenpei999@gmail.com>
2023-11-18 00:18:10 +0900

    feat footer

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
 </main>
 
-<footer class="c-background--gold py-9  px-30">
+<footer class="c-background--gold py-9 px-30 text-white">
   <nav class="footer-logo">
     <a href="#">
       <img src="<?php echo do_shortcode('[image_path name="logo" extension="png"]') ?>" alt="大学入試小論文指導カンザキジュク" width="175px" height="25px">
     </a>
   </nav>
-  <div class="flex">
+  <div class="flex justify-between">
     <address>
       〒158-0094<br>
       東京都世田谷区玉川4丁目12-15<br>
@@ -14,17 +14,15 @@
       TEL: <a href="tel:+810364310415">03−6431−0415</a>
       (<time datetime="13:00">平日13:00</time>～<time datetime="17:00">17:00</time>)
     </address>
-    <ul>
-      <li>
-        <a href="">
-          <img src="" alt="">
-        </a>
-      </li>
-      <li><a href=""></a></li>
-      <li><a href=""></a></li>
-      <li><a href=""></a></li>
-    </ul>
-
+    <div class="flex flex-col">
+      <ul class="flex justify-around gap-16">
+        <li><a href=""><img src="<?php echo do_shortcode('[image_path name="instagram" extension="png"]') ?>" alt="instagramはこちら"></a></li>
+        <li><a href=""><img src="<?php echo do_shortcode('[image_path name="VectorX" extension="png"]') ?>" alt="Xはこちら"></a></li>
+        <li><a href=""><img src="<?php echo do_shortcode('[image_path name="facebook" extension="png"]') ?>" alt="facebookはこちら"></a></li>
+        <li><a href=""><img src="<?php echo do_shortcode('[image_path name="note" extension="png"]') ?>" alt="noteはこちら"></a></li>
+      </ul>
+      <p>©️ 2023 カンザキメソッド Inc. All Right Reserved.</p>
+    </div>
   </div>
 
 </footer>
